6e864a9fabcf89221ca322839f6a35eed740f6bc
kriang <kriang@akagu.co>
2018-04-27 14:42:54 +0700

    Removed unused files from package.json

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public')
+const publicPath = path.join(__dirname, '..', '/public')
 
 app.use(express.static(publicPath));
 
-app.listen(4000, () => {
-    console.log('Server is up at port 4000. Please visit http://localhost:4000 to view the app.');
+app.listen(4001, () => {
+    console.log('Server is up at port 4001. Please visit http://localhost:4001 to view the app.');
 });
